5b2023b4e255a2a2e6cd0407c5d9fa48c99081a6
Jialin Yang <43101089+Skooooo@users.noreply.github.com>
2022-04-06 16:22:25 -0300

    update

---
+++ Pedestrainer/Assets/Scripts/GameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void startGame(){
+        SceneManager.LoadScene("Pedestrian");
+        SceneManager.UnloadSceneAsync("GameController");
+    }
+
+    public void quitGame(){
+        Application.Quit();
+    }
+
+    public void loadScene(){
+        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
