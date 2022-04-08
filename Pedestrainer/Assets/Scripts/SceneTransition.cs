8cac956ce6d845f93d5f5a51de5a4665d4999f3e
ryh <renyuhan519@gmail.com>
2022-04-08 20:18:41 -0300

    update move

---
+++ Pedestrainer/Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player") && !collision.isTrigger)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
