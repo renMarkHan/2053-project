3b138795fa8b605c7c72d7a1653265b28d43b8a0
Jialin Yang <43101089+Skooooo@users.noreply.github.com>
2022-04-09 23:09:15 -0300

    Barrier Jialin

--- Pedestrainer/Assets/Scripts/BarrierController.cs
+++ Pedestrainer/Assets/Scripts/BarrierController.cs
@@ -5,17 +5,23 @@ using UnityEngine.Tilemaps;
 
 public class BarrierController : MonoBehaviour
 {
+    private Tilemap map;
+    private Vector3 pPos;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        map = GetComponent<Tilemap>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnCollisionEnter2D(Collision2D other) {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        pPos = player.transform.position;
+        pPos.y -= 0.5f;
+        if(other.gameObject == player){
+            map.SetTile(map.WorldToCell(pPos), null);
+        }
     }
 }
 
